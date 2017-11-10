6d3bce7b23177b74051949604ce6e45c1dbdd944
Ilya Posov <iposov@gmail.com>
2017-11-10 18:03:29 +0300

    some addremove

--- ImagesUnion/src/ru/ipo/imageunion/JP_02.java
+++ ImagesUnion/src/ru/ipo/imageunion/JP_02.java
@@ -1,15 +1,13 @@
 package ru.ipo.imageunion;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class UA_04 {
+public class JP_02 {
 
     //TODO make this also command line arguments
     private static final String TASKS_DIRECTORY = "src";
-    private static final String TASK_NAME = "UA-04";
+    private static final String TASK_NAME = "JP-02";
 
     public static void main(String[] args) throws IOException {
         Pics pics = new Pics(TASK_NAME, new File(TASKS_DIRECTORY));
