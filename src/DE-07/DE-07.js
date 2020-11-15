abedec21d565ee738280dcda7a22dac8a83b61f4
Ilya Posov <iposov@gmail.com>
2020-11-15 07:57:44 +0300

    finished DE-07

--- src/DE-07/DE-07.js
+++ src/DE-07/DE-07.js
@@ -37,9 +37,14 @@ export class Task {
 
         this.ddlib = new App(container, WIDTH, HEIGHT, pictures, places, true);
         this.ddlib.setDragendCallback(() => {
-               console.log(this.ddlib.getOutput());
+               this.updateText();
         });
 
+        this.textDiv = document.createElement("div");
+        this.textDiv.style.marginLeft = "4px";
+        document.getElementById(container).appendChild(this.textDiv);
+
+        this.updateText();
     }
 
     reset() {
@@ -63,10 +68,65 @@ export class Task {
     }
 
     loadSolution(solution) {
-        return this.ddlib.loadSolution(solution);
+        let result = this.ddlib.loadSolution(solution);
+        this.updateText();
+        return result;
     }
 
     getAnswer() { //-1 no answer, 0 wrong, 1 correct, 2 - server check
-        return 2;
+        return this.scores() === 32 ? 1 : 0;
+    }
+
+    scores() {
+        let o = this.ddlib.getOutput();
+
+        if (!o["h00"])
+            return -1;
+
+        let s = 0;
+        let sc = (c1, c2) => {
+            if (c1 === 'r' && c2 === 'y' || c1 === 'y' && c2 === 'r')
+                return 3;
+            if (c1 === 'o' && c2 === 'y' || c1 === 'y' && c2 === 'o')
+                return 1;
+            return 0;
+        };
+
+        let colors = {o: 0, y: 0, r: 0};
+
+        for (let i = 0; i < 3; i++)
+            for (let j = 0; j < 3; j++) {
+                let c1 = o["h" + i + j];
+                if (c1 === -1)
+                    continue;
+                c1 = c1[0];
+                colors[c1]++;
+                if (j < 2) {
+                    let c2 = o["h" + i + (j + 1)];
+                    if (c2 !== -1)
+                        s += sc(c1, c2[0]);
+                }
+                if (i < 2) {
+                    let c2 = o["h" + (i + 1) + j];
+                    if (c2 !== -1)
+                        s += sc(c1, c2[0]);
+                }
+            }
+
+        if (colors['y'] === 0 || colors['r'] === 0 || colors['o'] === 0)
+            return -1;
+
+        return s;
+    }
+
+    updateText() {
+        let s = this.scores();
+        this.textDiv.style.color = s >= 0 ? "black" : "red";
+        let text;
+        if (s < 0)
+            text = 'Использованы не все цвета';
+        else
+            text = 'Очков: ' + s;
+        this.textDiv.innerText = text;
     }
 }
