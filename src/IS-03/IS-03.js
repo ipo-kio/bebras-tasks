f23febd83b2231c55fa306834bb7a3144179e423
Ilya Posov <iposov@gmail.com>
2021-11-13 18:12:11 +0300

    !!!

--- src/IS-03/IS-03.js
+++ src/IS-03/IS-03.js
@@ -1,23 +1,16 @@
 import mouse_coordinates from "../lib/MouseCoordinates";
-import {Item} from "./item";
+import {Cell, Item, WINDOW_SKIP_X, WINDOW_SKIP_Y, WINDOW_X0, WINDOW_Y0} from "./item";
 
 const CANVAS_W = 485;
-const CANVAS_H = 300;
+const CANVAS_H = 400;
+const DRESSER_W = 485;
+const DRESSER_H = 300;
 
-const M = 3;
-const N = 5;
-const WINDOW_W = 100;
-const WINDOW_H = 100;
-const WINDOW_X0 = 100;
-const WINDOW_Y0 = 100;
-const WINDOW_SKIP_X = 100;
-const WINDOW_SKIP_Y = 100;
-
-const ITEMS = [
-    ['heart', 'tree', 'cross', 'moon', 'heart'],
-    ['cross', 'pentagon', 'key', 'tree', 'star'],
-    ['tree', 'diamond', 'star', 'heart', 'triangle'],
-]
+const DRESSER_ITEMS = [
+    [['diamond', 'heart'], ['star', 'tree'], ['heart', 'cross'], ['star', 'moon'], ['circle', 'heart']],
+    [['pentagon', 'cross'], ['circle', 'pentagon'], ['moon', 'key'], ['diamond', 'tree'], ['triangle', 'star']],
+    [['cross', 'tree'], ['heart', 'diamond'], ['square', 'star'], ['triangle', 'heart'], ['pentagon', 'triangle']]
+];
 
 export class Task {
 
@@ -26,6 +19,11 @@ export class Task {
     ctx;
     bg;
     loaded;
+
+    items_list;
+
+    cursor_x = -1;
+    cursor_y = -1;
 
     //container - is an id of element
     constructor(container, images) {
@@ -40,18 +38,45 @@ export class Task {
         this.bg.onload = () => this.bgLoaded();
         
         this.items = [
-            new Item(this.bg, 'square', 140, 5, 2, 5),
-            new Item(this.bg, 'diamond', 165, 5, 2, 5),
-            new Item(this.bg, 'moon', 190, 5, 2, 5),
-            new Item(this.bg, 'star', 215, 5, 2, 5),
-            new Item(this.bg, 'tree', 240, 5, 2, 5),
-            new Item(this.bg, 'cross', 265, 5, 2, 5),
-            new Item(this.bg, 'circle', 290, 5, 2, 5),
-            new Item(this.bg, 'key', 0, 0, 6, 4),
-            new Item(this.bg, 'triangle', 315, 5, 2, 5),
-            new Item(this.bg, 'pentagon', 340, 5, 2, 5),
-            new Item(this.bg, 'heart', 365, 5, 2, 5)
+            new Item(this.bg, 'square', 140, 300, 25, 25),
+            new Item(this.bg, 'diamond', 165, 300, 25, 25),
+            new Item(this.bg, 'moon', 190, 300, 25, 25),
+            new Item(this.bg, 'star', 215, 300, 25, 25),
+            new Item(this.bg, 'tree', 240, 300, 25, 25),
+            new Item(this.bg, 'cross', 265, 300, 25, 25),
+            new Item(this.bg, 'circle', 290, 300, 25, 25),
+            new Item(this.bg, 'key', 0, 300, 140, 64),
+            new Item(this.bg, 'triangle', 315, 300, 25, 25),
+            new Item(this.bg, 'pentagon', 340, 300, 25, 25),
+            new Item(this.bg, 'heart', 365, 300, 25, 25)
         ];
+
+        this.cells = [];
+        this.all_cells = [];
+
+        let y0 = WINDOW_Y0;
+        for (let line of DRESSER_ITEMS) {
+            let items_line = [];
+            this.cells.push(items_line);
+
+            let x0 = WINDOW_X0;
+            for (let [from, to] of line) {
+                let cell = new Cell(x0, y0, from, to);
+                items_line.push(cell);
+                this.all_cells.push(cell);
+                x0 += WINDOW_SKIP_X;
+            }
+            y0 += WINDOW_SKIP_Y;
+        }
+
+        this.items_list = [this.get_item_by_name('circle')];
+    }
+
+    get_item_by_name(name) {
+        for (let real_item of this.items)
+            if (real_item.name === name)
+                return real_item;
+        return null;
     }
 
     bgLoaded() {
@@ -81,7 +106,7 @@ export class Task {
                 this.mouseleave(mouse_coordinates(this.canvas, e))
         });
         this.canvas.addEventListener('mousemove', (e) => {
-            if (e.buttons & 1 !== 0 && this.isEnabled())
+            if (this.isEnabled())
                 this.mousemove(mouse_coordinates(this.canvas, e))
         });
 
@@ -98,11 +123,15 @@ export class Task {
     }
 
     mouseleave(xy) {
-        //TODO mouseleave
+        this.cursor_x = -1;
+        this.cursor_x = -1;
     }
 
-    mousemove(xy) {
-        //TODO mosemove
+    mousemove({x, y}) {
+        this.cursor_x = x;
+        this.cursor_y = y;
+
+        this.redraw();
     }
 
 
@@ -110,6 +139,25 @@ export class Task {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.drawImage(this.bg, 0, 0, 485, 300, 0, 0, 485, 300);
+
+        //draw items list
+        let x0 = 30;
+        let y0 = DRESSER_H + 25;
+        for (let item of this.items_list) {
+            item.draw(this.ctx, x0, y0);
+            x0 += 30;
+            //TODO draw arrows from left to right
+        }
+        if (this.cursor_x >= 0 && this.cursor_y >= 0) {
+            // draw item under cursor
+            let last_item = this.items_list[this.items_list.length - 1];
+            last_item.draw(this.ctx, this.cursor_x, this.cursor_y);
+            // draw highlighting
+            for (let cell of this.all_cells) {
+                if (cell.hit_test(this.cursor_x, this.cursor_y) && cell.item_name_from === last_item.name)
+                    cell.draw_highlight(this.ctx);
+            }
+        }
     }
 
     reset() {
--- src/IS-03/item.js
+++ src/IS-03/item.js
@@ -1,11 +1,19 @@
+export const WINDOW_W = 62;
+export const WINDOW_H = 62;
+export const WINDOW_X0 = 40;
+export const WINDOW_Y0 = 34;
+export const WINDOW_SKIP_X = 85.5;
+export const WINDOW_SKIP_Y = 85;
+
 export class Item {
 
-    constructor(bg, x, y, w, h) {
+    constructor(bg, name, x, y, w, h) {
         this.bg = bg;
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
+        this.name = name;
         this.w2 = Math.trunc(w / 2);
         this.h2 = Math.trunc(h / 2);
     }
@@ -14,4 +22,29 @@ export class Item {
         ctx.drawImage(this.bg, this.x, this.y, this.w, this.h, cx - this.w2, cy - this.h2, this.w, this.h);
     }
 
+}
+
+export class Cell {
+    constructor(x, y, item_name_from, item_name_to) {
+        this.x = x;
+        this.y = y;
+        this.item_name_from = item_name_from;
+        this.item_name_to = item_name_to;
+    }
+
+    hit_test(x0, y0) {
+        // console.log(x0, y0, this.x, this.y, WINDOW_W, WINDOW_H, this.x <= x0 && x0 <= this.x + WINDOW_W && this.y <= y0 && y0 <= this.y + WINDOW_H);
+        return this.x <= x0 && x0 <= this.x + WINDOW_W && this.y <= y0 && y0 <= this.y + WINDOW_H;
+    }
+
+    draw_highlight(ctx) {
+        ctx.save();
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'yellow';
+
+        ctx.strokeRect(this.x, this.y, WINDOW_W, WINDOW_H);
+
+        ctx.restore();
+    }
 }
