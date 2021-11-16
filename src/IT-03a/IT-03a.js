cfc4ac6db596f8ac2e54af0fdf3fbeae096aa948
Ilya Posov <iposov@gmail.com>
2021-11-16 03:18:12 +0300

    create table

---
+++ src/IT-03a/IT-03a.js
@@ -0,0 +1,56 @@
+import {RectangleStatefulElement, SimpleStatesTask} from "../lib/SimpleStatesTask";
+
+const D = 40;
+const PAD = 4;
+
+export class Task extends SimpleStatesTask {
+    constructor(container, pictures) {
+        super(container, 6 * D + 2 * PAD, 5 * D + 2 * PAD);
+    }
+
+    createElements() {
+        return [
+            new Rect(this.ctx, 0, 0, 0, 1), //A
+            new Rect(this.ctx, 1, 0, 1, 0), //B
+            new Rect(this.ctx, 2, 0, 5, 1), //C
+            new Rect(this.ctx, 0, 2, 0, 2), //D
+            new Rect(this.ctx, 1, 1, 1, 2), //E
+            new Rect(this.ctx, 2, 2, 2, 4), //F
+            new Rect(this.ctx, 3, 2, 3, 3), //G
+            new Rect(this.ctx, 4, 2, 4, 3), //H
+            new Rect(this.ctx, 5, 2, 5, 4), //I
+            new Rect(this.ctx, 0, 3, 1, 3), //J
+            new Rect(this.ctx, 1, 4, 1, 4), //K
+            new Rect(this.ctx, 3, 4, 4, 4), //L
+            // new Rect(),
+            // new Rect(),
+            // new Rect(),
+            // new Rect(),
+            // new Rect(),
+            // new Rect(),
+            // new Rect(),
+            // new Rect(),
+            // new Rect(),
+        ];
+    }
+}
+
+const COLORS = ['white', 'red', 'blue'];
+
+class Rect extends RectangleStatefulElement {
+
+    constructor(ctx, x1, y1, x2, y2, color) {
+        super(ctx, 3, [1, 2, 0], PAD + x1 * D, PAD + y1 * D, (x2 - x1 + 1) * D, (y2 - y1 + 1) * D);
+    }
+
+    draw() {
+        this.ctx.save();
+        this.ctx.fillStyle = COLORS[this.state];
+        this.ctx.strokeStyle = '#AAA';
+        this.ctx.lineWidth = 1;
+        this.begin_outline_path();
+        this.ctx.fill();
+        this.ctx.stroke();
+        this.ctx.restore();
+    }
+}
